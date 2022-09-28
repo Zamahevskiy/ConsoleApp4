136847c95bddd69ccabf13743fea703182f377c9
Zamahevskiy <112929836+Zamahevskiy@users.noreply.github.com>
2022-09-28 18:17:43 +0300

    Update Категория.cs

--- ConsoleApp4/Категория.cs
+++ ConsoleApp4/Категория.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp4
 {
-     class Категория : Товар
+     class Category : Goods
     {
-        public int MasivTovarov { get; set; }
-        public void uКатегория()
+       
+        public int ArrayOfGoods { get; set; }
+        public void uCategory()
         {
-            Console.WriteLine($"Имя: {name} Масив товаров: {MasivTovarov}");
+            Console.WriteLine($"Имя: {name} Масив товаров: {ArrayOfGoods}");
         }
     }
 }
